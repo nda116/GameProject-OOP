3c02d03f7570b06a4b7f5fcf824b82d2d556717b
Nguyễn Đức Anh <24022763@vnu.edu.vn>
2025-10-07 00:16:03 +0700

    Update Ball.java

--- src/main/java/com/arkanoid/objects/Ball.java
+++ src/main/java/com/arkanoid/objects/Ball.java
@@ -1,17 +1,42 @@
 package com.arkanoid.objects;
 
 import com.arkanoid.core.MovableObject;
+import com.arkanoid.core.GameObject;
 
-public class Ball extends MovableObject{
+public class Ball extends MovableObject {
     private double speed;
     private double directionX;
     private double directionY;
+    private final double screenWidth;
+    private final double screenHeight;
 
-    public Ball(double x, double y, double radius, double speed) {
+    public Ball(double x, double y, double radius, double speed,
+                double screenWidth, double screenHeight) {
         super(x, y, radius * 2, radius * 2, 0, 0);
         this.speed = speed;
-        this.directionX = 0;
-        this.directionY = 0;
+        this.screenWidth = screenWidth;
+        this.screenHeight = screenHeight;
+
+        directionX = 0;
+        directionY = 1;
+        updateVelocity();
+    }
+
+    /**
+     * Updates the velocity (dx, dy) based on speed and direction.
+     */
+    private void updateVelocity() {
+        dx = speed * directionX;
+        dy = speed * directionY;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+        updateVelocity();
     }
 
     public double getDirectionX() {
@@ -20,6 +45,7 @@ public class Ball extends MovableObject{
 
     public void setDirectionX(double directionX) {
         this.directionX = directionX;
+        updateVelocity();
     }
 
     public double getDirectionY() {
@@ -28,5 +54,6 @@ public class Ball extends MovableObject{
 
     public void setDirectionY(double directionY) {
         this.directionY = directionY;
+        updateVelocity();
     }
 }
