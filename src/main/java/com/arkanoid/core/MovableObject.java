c5d7edc43a1939c0c2cbe5793924a65a442fc262
Nguyễn Đức Anh <24022763@vnu.edu.vn>
2025-10-06 08:23:11 +0700

    add all

---
+++ src/main/java/com/arkanoid/core/MovableObject.java
@@ -0,0 +1,32 @@
+package com.arkanoid.core;
+
+public abstract class MovableObject extends GameObject{
+    private double dx;
+    private double dy;
+
+    public MovableObject(double x, double y, double width, double height, double dx, double dy) {
+        super(x, y, width, height);
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public void setDx(double dx) {
+        this.dx = dx;
+    }
+
+    public double getDy() {
+        return dy;
+    }
+
+    public void setDy(double dy) {
+        this.dy = dy;
+    }
+
+    public void move() {
+
+    }
+}
